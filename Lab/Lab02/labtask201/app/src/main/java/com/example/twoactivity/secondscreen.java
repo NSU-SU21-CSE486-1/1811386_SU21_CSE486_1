ed7003261d6e1b90ba3866e9e646d9cd8de02fee
ahadulislam3 <73934957+ahadulislam3@users.noreply.github.com>
2021-07-08 16:30:15 +0600

    lab task 2 done

--- Lab/Lab02/labtask201/app/src/main/java/com/example/twoactivity/secondscreen.java
+++ Lab/Lab02/labtask201/app/src/main/java/com/example/twoactivity/secondscreen.java
@@ -2,13 +2,16 @@ package com.example.twoactivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
 
 public class secondscreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_secondscree);
+        setContentView(R.layout.activity_main2);
     }
 }
