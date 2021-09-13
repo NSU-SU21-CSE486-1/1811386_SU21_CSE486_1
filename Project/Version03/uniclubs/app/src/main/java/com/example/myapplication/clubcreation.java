3e07237ecb073bf0fedb2baa02fbdb7ea6c9aa25
ahadul <73934957+ahadulislam3@users.noreply.github.com>
2021-09-14 03:15:44 +0600

    .

---
+++ Project/Version03/uniclubs/app/src/main/java/com/example/myapplication/clubcreation.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class clubcreation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.clubcreation);
+    }
+}
